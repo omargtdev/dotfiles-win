065e547c702bd0978718030334816eeaf3f3a856
Omar Gutierrez <omargtdev@gmail.com>
2025-04-02 17:15:55 -0500

    validate baseDir of Powershell profile

--- pwsh/setup.ps1
+++ pwsh/setup.ps1
@@ -2,5 +2,9 @@
 
 $baseDir = Split-Path -Parent $PROFILE
 
+if (-Not (Test-Path -Path $baseDir)) {
+    mkdir $baseDir
+}
+
 Copy-Item -Path *.ps1 -Exclude setup.ps1 -Destination $baseDir
 Copy-Item -Path ./temp_scripts -Recurse -Force -Destination $baseDir
